4bf39be847153c14f4b207775a0a770f2557c780
Nigel Baillie <nigel@baillie.dev>
2024-07-16 16:35:50 +0900

    better hx setup

--- container-setup/inner.sh
+++ container-setup/inner.sh
@@ -5,6 +5,11 @@ set -e
 apt-get update
 apt-get install -y fish
 
+mkdir -p "$HOME/.config/helix"
+echo 'theme = "vim_dark_high_contrast"' > "$HOME/.config/helix/config.toml"
+echo '[editor]'                         >> "$HOME/.config/helix/config.toml"
+echo 'true-color = true'                >> "$HOME/.config/helix/config.toml"
+
 if command -v &> /dev/null; then
   echo hx already installed. done
 else
@@ -13,8 +18,6 @@ else
   cd ~
   wget "https://github.com/helix-editor/helix/releases/download/24.07/$helix.tar.xz"
   tar -xf "$helix.tar.xz"
-
-  mkdir -p "$HOME/.config/helix"
 
   ln -s "$PWD/$helix/hx" /usr/bin/hx
   ln -s "$PWD/$helix/runtime" "$HOME/.config/helix/runtime"

df96433e7acf42234e5909ed29103a9f7b8d4c15
Nigel Baillie <nigel@baillie.dev>
2024-08-29 21:00:24 +0900

    do npm ci for coc in setup script

--- nvim-setup.fish
+++ nvim-setup.fish
@@ -1,11 +1,13 @@
 #!/usr/bin/fish
 
 if test -d $HOME/.config/nvim
-  mv $HOME/.config/nvim $HOME/.config/nvim-bak
+    mv $HOME/.config/nvim $HOME/.config/nvim-bak
 end
 
 git submodule update --init
 git submodule update
 ln -s $PWD/nvim $HOME/.config/nvim
+
+sh -c "cd $PWD/nvim/pack/nvim/start/coc.nvim && npm ci"
 
 echo done
